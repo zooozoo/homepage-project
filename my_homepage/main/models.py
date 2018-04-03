aa8146bc0428958d644a6bb61e69d4186b3dfced
zooozoo <shz0309@gmail.com>
2018-04-03 23:00:25 +0900

    1크롤링 한지 한시간 이상이면 다시 크롤링 해오게끔 뷰에서 기능 만듦

--- my_homepage/main/models.py
+++ my_homepage/main/models.py
@@ -6,7 +6,13 @@ class NaverNews(models.Model):
     title = models.CharField(max_length=100)
     created_time = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.title
+
 
 class DaumNews(models.Model):
     title = models.CharField(max_length=100)
     created_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
--- my_homepage/main/views.py
+++ my_homepage/main/views.py
@@ -1,13 +1,26 @@
+from datetime import datetime, timezone, timedelta
+
 from django.shortcuts import render
+
+from main.models import NaverNews, DaumNews
 from .utils import naver_news_title, daum_news_title
 
 
 # Create your views here.
 def index_page(request):
-    naver_news_list = naver_news_title()
-    daum_news_list = daum_news_title()
+    if NaverNews.objects.last() is None or timedelta(hours=1) < NaverNews.objects.last().created_time - datetime.now(timezone.utc):
+        NaverNews.objects.all().delete()
+        DaumNews.objects.all().delete()
+        naver_news_list = naver_news_title()
+        daum_news_list = daum_news_title()
+        for item in naver_news_list:
+            NaverNews.objects.create(title=item)
+        for item in daum_news_list:
+            DaumNews.objects.create(title=item)
+    naver = NaverNews.objects.all()
+    daum = DaumNews.objects.all()
     context = {
-        'naver': naver_news_list,
-        'daum': daum_news_list,
+        'naver': naver,
+        'daum': daum,
     }
     return render(request, 'main/index.html', context=context)
